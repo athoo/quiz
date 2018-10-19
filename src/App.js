88b140d66828527116f5e5607d32868f53c74156
pingjing <athoo@sina.com>
2018-10-19 17:37:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button} from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends Component {
-    constructor(props, context) {
-        super(props, context);
 
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
         this.handleChange = this.handleChange.bind(this);
-
-        this.state = {
-            value: ''
-        };
-    }
-
-    getValidationState() {
-        const length = this.state.value.length;
-        if (length <= 2) return 'success';
-        else if (length > 2) return 'warning';
-        return null;
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
-        this.setState({ value: e.target.value });
-        console.log(this);
+        this.setState({value:e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("run submit function");
+        console.log(this.state.value);
+        axios.get(`http://localhost:5000/getQuestions/${this.state.value}`)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
     }
 
     render() {
@@ -31,7 +32,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
               <h1>Welcome to NOAH study</h1>
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <FormGroup>
                   <ControlLabel>Please select your Participant number:</ControlLabel>
                   <FormControl

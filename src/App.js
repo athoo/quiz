c6568d343cc6132acbd20e1b1bd5788a68cf6041
杨平京 <yangpj@ruyi.ai>
2018-10-21 18:30:42 -0500

    testing1

--- src/App.js
+++ src/App.js
@@ -17,83 +17,151 @@ import axios from 'axios';
 
 // let userId;
 
-function WelcomePage(props){
-    return (
-        <header className="App-header">
-            <h1>Welcome to NOAH study</h1>
-            <form onSubmit={props.handleSubmit}>
-                <FormGroup>
-                    <ControlLabel>Please select your Participant number:</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={props.value}
-                        placeholder="Enter number"
-                        onChange={props.handleChange}
-                    />
-                </FormGroup>
-                <Button bsStyle="primary" type="submit" className="Start">Start</Button>
-            </form>
+function Background(props){
+    return(
+        <header className={"App-header"}>
+        {props.children}
         </header>
-    );
+    )
 }
 
-
-class App extends Component {
-
-    constructor(props){
+class Question extends Component {
+    constructor(props) {
         super(props);
-        this.state = {
-            value: '',
-            quizzes: {},
-            isLoggedIn: false
-        };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+
     }
 
     handleChange(e) {
-        this.setState({value:e.target.value});
+        e.preventDefault();
+        this.props.response(e.target.value);
     }
 
-    handleSubmit(user) {
-        // e.preventDefault();
-        console.log("run submit function");
-        // console.log(e.props.value);
-        console.log(this);
-        axios.get(`http://localhost:5000/getQuestions/${user}`)
-            .then(res => {
-                // console.log(res);
-                console.log(res.data.task1.system);
-                const quizzes = res.data;
-                this.setState(state => ({
-                    quizzes: quizzes
-                }))
-                console.log(this.state.quizzes);
-            })
+    render(){
+
+        return(
+            <Background>
+                <h1>{this.props.title}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Fill your response in the box below</label>
+                    <input type="text" value={this.props.response} onChange={this.handleChange}></input>
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </Background>
+        )
+
     }
 
 
-    render() {
+}
 
-        const isloggedIn = this.state.isLoggedIn;
-        let content;
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {title:"Hello world",response:"Who are you"};
+    }
 
-        if (isloggedIn){
+    render(){
+        const title = this.state.title;
+        return(
+            <Background>
+                <Question title={title}>
 
-        }else{
-            content = <WelcomePage
-                handleSubmit={(this.value) => this.handleSubmit}
-                value={this.value}
-            />
-        }
-
-        return (
-            <div className="App">
-                {content}
-            </div>
-        );
+                </Question>
+            </Background>
+        )
     }
 }
+
+// function WelcomePage(props){
+//     return(
+//         <h1>Welcome to NOAH Study</h1>
+//         <form onSubmit=
+//     )
+// }
+
+
+//
+// function WelcomePage(props){
+//     return (
+//         <header className="App-header">
+//             <h1>Welcome to NOAH study</h1>
+//             <form onSubmit={props.handleSubmit}>
+//                 <FormGroup>
+//                     <ControlLabel>Please select your Participant number:</ControlLabel>
+//                     <FormControl
+//                         type="text"
+//                         value={props.value}
+//                         placeholder="Enter number"
+//                         onChange={props.handleChange}
+//                     />
+//                 </FormGroup>
+//                 <Button bsStyle="primary" type="submit" className="Start">Start</Button>
+//             </form>
+//         </header>
+//     );
+// }
+
+//
+// class App extends Component {
+//
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             value: '',
+//             quizzes: {},
+//             isLoggedIn: false
+//         };
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//
+//     handleChange(e) {
+//         this.setState({value:e.target.value});
+//     }
+//
+//     handleSubmit(user) {
+//         // e.preventDefault();
+//         console.log("run submit function");
+//         // console.log(e.props.value);
+//         console.log(this);
+//         axios.get(`http://localhost:5000/getQuestions/${user}`)
+//             .then(res => {
+//                 // console.log(res);
+//                 console.log(res.data.task1.system);
+//                 const quizzes = res.data;
+//                 this.setState(state => ({
+//                     quizzes: quizzes
+//                 }))
+//                 console.log(this.state.quizzes);
+//             })
+//     }
+//
+//
+//     render() {
+//
+//         const isloggedIn = this.state.isLoggedIn;
+//         let content;
+//
+//         if (isloggedIn){
+//
+//         }else{
+//             content = <WelcomePage
+//                 handleSubmit={(this.value) => this.handleSubmit}
+//                 value={this.value}
+//             />
+//         }
+//
+//         return (
+//             <div className="App">
+//                 {content}
+//             </div>
+//         );
+//     }
+// }
 
 
 
